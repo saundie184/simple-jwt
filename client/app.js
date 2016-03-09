333f34b9a3afc125a9dd86d5c466fc5ed37ff2bf
Saundie Weiss <saundieweiss@gmail.com>
2016-03-09 12:01:27 -0700

    Added JWTs

--- client/app.js
+++ client/app.js
@@ -1,11 +1,28 @@
-$('#login').submit(function(){
+'use strict';
 
+//use submit so that you can use click or enter to submit form
+$('#login').submit(function() {
+  event.preventDefault();
+  var data = $('#login').serialize();
+  // console.log(data);
+  $.post('//localhost:3000/auth', data, function() {
+    //savesession/ local storage to save the token
+
+  });
 });
 
-$('#logout').click(function(){
-
+$('#logout').click(function() {
+  //client deletes session/local storage
 });
 
-$('#secure_request').click(function(){
-
+$('#secure_request').click(function() {
+  //check this line:
+var jwt = sessionStorage.get();
+  //check session storage for jwt
+  //get request that you need to send this
+  $.ajax('/api/somepage', {
+    headers: {
+      Authorization: 'Bearer ' + jwt
+    }
+  });
 });
--- server/app.js
+++ server/app.js
@@ -6,13 +6,15 @@ var express = require('express');
 var bodyParser = require('body-parser');
 
 var cors = require('cors');
+//Express middleware
 var expressJwt = require('express-jwt');
+//decyrpt and encyrpyt
 var jwt = require('jsonwebtoken');
 
 
 
 var app = express();
-
+//should put this in a .env
 var secret = 'this is my secret';
 var config = {
  secret: secret
@@ -27,7 +29,12 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, '../www')));
 
 app.post('/auth', function(req, res){
+  //to see how the post request is working right:
+  // console.log(req.body);
 
+//normally you would hash the password and check the users name
+
+if(req.body.username === 'hello'){
   var user = {
     username: req.body.username || 'bill',
     id: 1
@@ -36,10 +43,15 @@ app.post('/auth', function(req, res){
   var expires = {
     expiresInMinutes: 60
   };
-
+//generate token
   var token = jwt.sign(user, secret, expires);
-
+//send as json
   res.json({ token: token});
+} else {
+  //if the user is not hello, send error
+  res.status(401).send('Credentials are bad!');
+}
+
 });
 
 app.get('/api/resource', function(req, res){
